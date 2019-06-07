9e76a985738f21d330c916063cf3b8a6c09fa119
silas <silas@helloample.com>
2019-06-07 10:32:53 -0400

    add jumbotron

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,17 +24,26 @@ const IndexPage = ({data}) => {
   return (
     <Layout>
       <SEO title="Home" />
-      <h1>Hi people</h1>
-      <p>Check this shit out!</p>
-      <div className="blog-container"> 
-
+      <div className="jumbotron">
+        <div className="jt-text">
+          <h2>Gatsby Article</h2>
+          <p>June 6, 2019</p>
+        </div>
+        <div className="jt-img">
+          <img src="./images/gatsby-astronaut.png" alt="featured blog" />
+        </div>
+      </div>
+      <div className="wrapper">
+        <h1>Hi people</h1>
+        <p>Check this shit out!</p>
+        <div className="blog-container"> 
           {articles}
-          
+        </div>
+        <div style={{ maxWidth: `300px`, margin: `1.45rem 0` }}>
+          <Image />
+        </div>
+        <Link to="/page-2/">Go to page 2</Link>
       </div>
-      <div style={{ maxWidth: `300px`, margin: `1.45rem 0` }}>
-        <Image />
-      </div>
-      <Link to="/page-2/">Go to page 2</Link>
     </Layout>
   );
 }
