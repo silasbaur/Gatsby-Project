41d7f5928583465936fd08319049f21a40afa8cf
Silas Baur <silasbaur@gmail.com>
2019-06-26 14:18:21 -0400

    change site name to blog spot

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 console.log(`${__dirname}/data/articles`);
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Stuff`,
+    title: `Blog Spot`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
